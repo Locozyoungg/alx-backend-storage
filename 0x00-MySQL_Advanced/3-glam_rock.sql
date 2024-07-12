0500d554f2f690ed02f6f1ccee149479882d4c99
Locozyoungg <collaustine27@gmail.com>
2024-07-13 02:41:21 +0300

    glam

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,18 +1,7 @@
--- Script to list Glam rock bands ranked by their longevity
 
--- Import the table dump first
--- $ cat metal_bands.sql | mysql -uroot -p holberton
-
-SELECT 
-    name AS band_name, 
-    CASE 
-        WHEN split IS NULL THEN 2022 - formed
-        ELSE split - formed
-    END AS lifespan
-FROM 
-    metal_bands
-WHERE 
-    main_style = 'Glam rock'
-ORDER BY 
-    lifespan DESC;
+-- ranked by their longevity
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE "%Glam rock%"
+ORDER BY lifespan DESC
 
