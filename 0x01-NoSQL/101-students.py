249b85510c84bad0d87199642e5eb7fa68506dc0
Locozyoungg <collaustine27@gmail.com>
2024-07-17 02:21:27 +0300

    students

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,13 @@
+def top_students(mongo_collection):
+    """ Returns all students sorted by average score """
+    pipeline = [
+        {
+            '$project': {
+                'name': 1,
+                'topics': 1,
+                'averageScore': { '$avg': '$topics.score' }
+            }
+        },
+        { '$sort': { 'averageScore': -1 } }
+    ]
+    return list(mongo_collection.aggregate(pipeline))
