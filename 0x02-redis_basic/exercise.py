7e5dbe4f40ce209d5e1339fe370feb010ee00c07
Locozyoungg <collaustine27@gmail.com>
2024-07-18 02:14:14 +0300

    exr4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,6 +17,19 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+def call_history(method: Callable) -> Callable:
+    """Decorator to store the history of inputs and outputs for a function"""
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper function to store inputs and outputs in Redis"""
+        input_key = f"{method.__qualname__}:inputs"
+        output_key = f"{method.__qualname__}:outputs"
+        self._redis.rpush(input_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(output))
+        return output
+    return wrapper
+
 class Cache:
     def __init__(self):
         """Initialize the Cache instance"""
@@ -24,6 +37,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in Redis with a random key"""
         key = str(uuid.uuid4())
@@ -46,3 +60,4 @@ class Cache:
     def get_int(self, key: str) -> Optional[int]:
         """Retrieve an integer from Redis"""
         return self.get(key, lambda d: int(d))
+
--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -3,7 +3,6 @@
 
 from exercise import Cache
 
-# Initialize the Cache
 cache = Cache()
 
 # Test case 1: Storing and retrieving binary data
@@ -50,3 +49,17 @@ key_int = cache.store(data_int)
 
 assert cache.get_str(key_str) == data_str
 assert cache.get_int(key_int) == data_int
+
+# Test case for call_history
+s1 = cache.store("first")
+print(s1)
+s2 = cache.store("secont")
+print(s2)
+s3 = cache.store("third")
+print(s3)
+
+inputs = cache._redis.lrange(f"{cache.store.__qualname__}:inputs", 0, -1)
+outputs = cache._redis.lrange(f"{cache.store.__qualname__}:outputs", 0, -1)
+
+print("inputs: {}".format(inputs))
+print("outputs: {}".format(outputs))
